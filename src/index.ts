83ff4418b4a457de40a9ddf14661d9015373ad9d
Adam Ure <adamqure@icloud.com>
2023-11-09 19:22:37 -0700

    Support /campaigns/generate endpoint (#37)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,16 @@ const index = express.Router();
 
 const generatorService = new GeneratorService();
 
+index.post('/campaigns/generate', function(req, res) {
+    console.log(req.body)
+    const body = req.body
+    if (Object.keys(body).length === 0) {
+        res.status(400).send('No input provided');
+    } else {
+        res.status(200).send()
+    }
+});
+
 /**
  * 
  * Handle GET campaign/{id}
@@ -12,11 +22,12 @@ const generatorService = new GeneratorService();
  * @returns A campaign with that id number
  */
 index.get('/campaign/:id', function(req, res) {
-    var id = req.params.id;
-    if (id === "none") {
+    let id = req.params.id;
+
+    if (id.toString() === "none") {
         res.status(404).send()
     } else {
-        res.status(200).json(JSON.stringify(generatorService.getCampaign(id))).send()
+        res.status(200).json(JSON.stringify(generatorService.getCampaign(id.toString()))).send()
     }
 });
 
