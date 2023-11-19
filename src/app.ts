411d81d00f7267299df5c70e7392cdf70f68d895
Adam Ure <adamqure@icloud.com>
2023-11-18 19:08:31 -0700

    Default port change to 8080

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", index);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
