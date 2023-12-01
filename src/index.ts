3c5c7141b346dd5e9c49bc5285050fb5924683de
Adam Ure <adamqure@icloud.com>
2023-11-30 22:27:24 -0700

    Handle preflight request with CORS

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import express, { response } from "express";
+import { GeneratorService } from "./services/generator/generator-service";
+import { CampaignFormValidator } from "./services/form-validator/form-validator";
+import { CampaignFormResponse } from "./models/campaign-form-response";
+
+var cors = require('cors')
+const index = express.Router();
+
+const generatorService = new GeneratorService();
+
+index.options('/campaigns/generate', cors())
+index.post('/campaigns/generate', cors(), async function(req, res) {
+    try {
+        console.log("Received generate request...")
+        res.removeHeader('Access-Control-Allow-Origin')
+        res.set('Access-Control-Allow-Origin', '*');
+        res.set('Access-Control-Allow-Methods', 'POST')
+    
+        // console.log(req.body)
+        // const body = req.body
+    
+        // if (Object.keys(body).length === 0) {
+        //     return res.status(400).send('No input provided');
+        // }
+        
+        // const validator = new CampaignFormValidator()
+        // const formInput = body as CampaignFormResponse
+
+        // validator.validateForm(formInput)
+        
+        // const id = await generatorService.createCampaign(formInput);
+        // const responseBody = { id };
+
+        // console.log("Sending generate campaign response body: " + responseBody)
+        res.status(200).send(JSON.stringify({"Test": "Test"}))
+    } catch(e) {
+        console.log("Invalid input form: " + e)
+        res.status(400).send("Invalid input parameters")
+    }
+});
+
+index.get('/campaigns/generate', cors(), async function(req, res) {
+    console.log("Received generate request...")
+    res.removeHeader('Access-Control-Allow-Origin')
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'POST')
+    res.status(200).send("No cors")
+});
+
+/**
+ * 
+ * Handle GET campaign/{id}
+ *
+ * @param id - A string id representing the generated campaign
+ * @returns A campaign with that id number
+ */
+index.get('/campaign/:id', cors(), function(req, res) {
+    res.removeHeader('Access-Control-Allow-Origin')
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET');
+    let id = req.params.id;
+
+    if (id.toString() === "none") {
+        res.status(404).send()
+    } else {
+        res.status(200).json(JSON.stringify(generatorService.getCampaign(id.toString()))).send()
+    }
+});
+
+export { index };
