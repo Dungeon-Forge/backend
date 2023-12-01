dd6c3df5211ea1a7c7fb99b7066c5d62cc2b0892
Adam Ure <adamqure@icloud.com>
2023-11-30 22:33:04 -0700

    Post generation cors issues (#60)
    * Test generator service causing CORS issues
    
    * Remove CORS header prior to setting in map
    
    * Remove Everything to test cors
    
    * Handle preflight request with CORS
    
    * Add preflight

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+app.options('*', cors())
 app.use("/", index);
 
 const port = process.env.PORT || 8080;
