84eebbfdbdfc844a7acd49f6e05cd099606a7734
Adam Ure <adamqure@icloud.com>
2023-12-08 19:02:03 -0700

    Check if writeFile has a bug (#82)

--- src/services/generator/generator-service.ts
+++ src/services/generator/generator-service.ts
@@ -29,7 +29,8 @@ export class GeneratorService {
         .then(async (content) => {
           logger.log("Writing data to file...")
           try {
-            await fsPromises.writeFile(this.campaignFile, content) 
+            await fsPromises.unlinkFile(this.campaignFile)
+            await fsPromises.appendFile(this.campaignFile, content) 
             logger.log("Data has been written to file successfully."); 
               
             if (shell.exec(`pagedjs-cli ${ this.campaignFile } -o ${ this.outputFile }`).code !== 0) {
